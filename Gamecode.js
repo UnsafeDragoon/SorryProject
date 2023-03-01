b6d6f36e6d4c74f108d1138fe0262278db7e1f9a
DonovanBurrola <donovan.burrola@student.cart.org>
2023-03-01 15:47:52 -0800

    a
    a

--- Gamecode.js
+++ Gamecode.js
@@ -1,5 +1,25 @@
+// Audio Functions
+
+// Javascript - Player Glow
+
+function muteAudio(
+    console.log('test')
+)
+
+
 let board = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44]
 
 class colorPiecep1qsazxx{
     
 }
+
+
+
+
+
+
+
+// Lobby coding for one-third class (also known as the map Selection)
+const blur = document.querySelector('one-third');
+const unblur = document.querySelector('map-text-container');
+
