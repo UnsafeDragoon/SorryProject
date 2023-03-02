dce5c245887d471857c977390497addb8a05eede
Carh00 <116219587+Carh00@users.noreply.github.com>
2023-03-02 15:48:32 -0800

    3/4 of Lobby Done layout, backg, and images
    -Finished player and map layout
    -Added Player amount dropdown option
    -Needing to making it link into javascript tho ^^
    -Need to add Start button/start game option
    -Background added and changed for nicer look
    -Added hover effect which adds a glow effect border to the selected item
    -Needs more HTML and CSS

--- Gamecode.js
+++ Gamecode.js
@@ -2,15 +2,17 @@
 
 // Javascript - Player Glow
 
-function muteAudio(
-    console.log('test')
-)
+// function muteAudi {
+//     console.log('test')
+// }
 
 
-let board = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44]
 
-class colorPiecep1qsazxx{
-    
+
+let board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+
+class colorPiecep1qsazxx {
+
 }
 
 
@@ -23,3 +25,36 @@ class colorPiecep1qsazxx{
 const blur = document.querySelector('one-third');
 const unblur = document.querySelector('map-text-container');
 
+
+// Lobby Javascript code
+
+
+// let el_up = document.getElementById("GFG_UP");
+// let el_down = document.getElementById("GFG_DOWN");
+// let str = "Click on button to change the background color";
+
+// el_up.innerHTML = str;
+
+// function changeColor(color) {
+//     document.body.style.background = color;
+// }
+
+// function gfg_Run() {
+//     changeColor('#cccc00');
+//     el_down.innerHTML = "Background Color changed To Yellow";
+// }      
+
+// function gfg_Run2() {
+//     changeColor('#0059b3');
+//     el_down.innerHTML = "Background Color changed To Blue";
+// }        
+
+// function gfg_Run3() {
+//     changeColor('#b23434');
+//     el_down.innerHTML = "Background Color changed To Red";
+// }        
+
+// function gfg_Run4() {
+//     changeColor('#408000');
+//     el_down.innerHTML = "Background Color changed To Green";
+// }        
