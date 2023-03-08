5500a2ff8be660d1d49e0d916f08ef06df09fee0
DonovanBurrola <donovan.burrola@student.cart.org>
2023-03-08 15:33:05 -0800

    map selection done

--- Gamecode.js
+++ Gamecode.js
@@ -1,33 +1,29 @@
 
-// Lobby code
 
 
+let maps = document.querySelectorAll('.one-third');
 
+maps.forEach(map => {
+  map.addEventListener('click', function() {
+    maps.forEach(mp => mp.classList.remove('java-one-third'));
+    map.classList.add('java-one-third');
+    let mapType = map.id
+  });
+});
 
-
-
-
-function mapSelect(event){
-  let label = event.target;
-  label.classList.add('java-one-third');
-}
-
-
-
-let matchSetUpConfirm = false;
+// let playerCount = document.getElementById('player-container').value
 
 function confirmMatch(){
-  console.log('worj')
+  if (map.id = mapOne){
+    console.log('1');
+  } else if (map.id = mapTwo){
+    console.log('2');
+  } else if (map.id = mapThree){
+    console.log('3');
+  } else{
+    console.log('skip')
+  }
 }
 
 
-
-
-let board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
-
-
-
-
-
-
-
+// let board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
