fda1cca707f5db2d93b55867a132c8d38ea0a5e7
Carh00 <116219587+Carh00@users.noreply.github.com>
2023-03-08 15:12:31 -0800

    Card working on click
    -JS making card appear when clicked on
    -JS also making card disappear when being clicked on
    -Needed CSS or something to fix size of card
    -

--- Gamecode.js
+++ Gamecode.js
@@ -1,33 +1,28 @@
+// Audio Functions
 
-// Lobby code
+// Javascript - Player Glow
 
-
-
-
-
-
-
-function mapSelect(event){
-  let label = event.target;
-  label.classList.add('java-one-third');
-}
-
-
-
-let matchSetUpConfirm = false;
-
-function confirmMatch(){
-  console.log('worj')
-}
+// function muteAudi {
+//     console.log('test')
+// }
 
 
 
 
 let board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
 
+class colorPiecep1qsazxx {
 
+}
 
+// const pickUpCard = document.getElementById('draw-card');
 
+function show() {
+    const cards = document.getElementById('taking');
+    cards.classList.add('active');
+}
 
-
-
+function dontShow() {
+    const gone = document.getElementById('taking')
+    gone.classList.remove('active')
+}
