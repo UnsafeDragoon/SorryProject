8f66f805096fa30ee588c36af8d02d32371de51a
michaelmarkus4690 <116219341+michaelmarkus4690@users.noreply.github.com>
2023-03-11 12:21:09 -0800

    Code organization

--- Gamecode.js
+++ Gamecode.js
@@ -1,4 +1,5 @@
-//  enter fullscreen
+
+//  Fullscreen Button Javascript
 var elem = document.documentElement;
 function openFullscreen() {
   if (elem.requestFullscreen) {
@@ -10,22 +11,28 @@ function openFullscreen() {
   }
 }
 
-// start page 
+//▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 
-// lobby setting up code
+
+// Mute Button Javascript
+
+//▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+// Start Page Javascript
+
+//▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+
+// Lobby Javascript
+
+// Player-Highlight
 
 let previousSelectedOption = 0;
-
 function getPlayerCount() {
   let selectedOption = parseInt(document.getElementById("player-container").value);
-
-  // Unhighlight all boxes when selected option is changed
   if (selectedOption !== previousSelectedOption) {
     unhighlightAllPlayers();
   }
-
-  
-
   if (selectedOption === 1) {
     highlightPlayerOne();
   } else if (selectedOption === 2) {
@@ -41,26 +48,20 @@ function getPlayerCount() {
     highlightPlayerThree();
     highlightPlayerFour();
   }
-
   previousSelectedOption = selectedOption;
 }
-
 function highlightPlayerOne() {
   document.getElementById("playerone").style.boxShadow = "0 0 20px yellow";
 }
-
 function highlightPlayerTwo() {
   document.getElementById("playertwo").style.boxShadow = "0 0 20px blue";
 }
-
 function highlightPlayerThree() {
   document.getElementById("playerthree").style.boxShadow = "0 0 20px red";
 }
-
 function highlightPlayerFour() {
   document.getElementById("playerfour").style.boxShadow = "0 0 20px green";
 }
-
 function unhighlightAllPlayers() {
   document.getElementById("playerone").style.boxShadow = "none";
   document.getElementById("playertwo").style.boxShadow = "none";
@@ -68,23 +69,23 @@ function unhighlightAllPlayers() {
   document.getElementById("playerfour").style.boxShadow = "none";
 }
 
+// Player-Map Verification
 
-let board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
-
-class colorPiecep1qsazxx {
-
+function setMatchPlayers(count){
+  console.log(count)
 }
 
-// const pickUpCard = document.getElementById('draw-card');
+function setMatchMap(){
 
-function show() {
-    const cards = document.getElementById('taking');
-    cards.classList.add('active');
-}
-
-function dontShow() {
-    const gone = document.getElementById('taking')
-    gone.classList.remove('active')
+  if (mapType === 'mapOne'){
+    setUpMapOne();
+  } else if (mapType == 'mapTwo'){
+    setUpMapTwo();
+  } else if (mapType == 'mapThree'){
+    setUpMapThree();
+  } else{
+    console.log('map not selected')
+  }
 }
 
 let maps = document.querySelectorAll('.one-third');
@@ -101,11 +102,9 @@ maps.forEach(map => {
 });
 
 
-// 3 map level stylings
-
 function setUpMapOne(){
     document.getElementById("bodyone").style.cssText='background-image: url(images/backgroundBoardOne.png);';
-  // background-image: url(images/backgroundBoardOne.png);  
+ 
 }
 
 function setUpMapTwo(){
@@ -116,7 +115,7 @@ function setUpMapThree(){
   console.log('map3')
 }
 
-// let playerCount = document.getElementById('player-container').value
+ue
 
 function confirmMatch(){
   if (playerCount.value == 0){
@@ -128,34 +127,25 @@ function confirmMatch(){
 
       window.location.href = "index.html";
 
-      // setMatchPlayers(playerCount.value);
-      // setMatchMap();
     }
-    // setMatchPlayers(playerCount.value)
   }
 }
+//▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 
 
 
-function setMatchPlayers(count){
-  console.log(count)
-}
-
-function setMatchMap(){
-  // if (playerCount.value == 'undefined' ){
-  //   console.log('eror')
-  // } else{
-  //   console.log('ss')
-  // }
-  if (mapType === 'mapOne'){
-    setUpMapOne();
-  } else if (mapType == 'mapTwo'){
-    setUpMapTwo();
-  } else if (mapType == 'mapThree'){
-    setUpMapThree();
-  } else{
-    console.log('map not selected')
-  }
-}
 
 
+
+// Game Code
+
+let board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+
+// Card Pickup
+
+//▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+
+
+
+//
