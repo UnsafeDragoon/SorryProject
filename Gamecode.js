aa4d1a9e072a41f40658134da0b09c11a8cde19d
DonovanBurrola <donovan.burrola@student.cart.org>
2023-03-16 13:55:10 -0700

    big html changes

--- Gamecode.js
+++ Gamecode.js
@@ -166,10 +166,16 @@ function setMatchMap(mapTypeFinal){
 
 
 
-let board = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44]
+let board = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43]
 
 
-
+function test(newPosition){
+  var gamePiece = document.createElement('div'); 
+  gamePiece.style.backgroundColor = "red";
+  gamePiece.style.height = "80%";
+  gamePiece.style.width = "80%";
+  document.getElementById(`${board[newPosition]}`).appendChild(gamePiece);           
+}
 
 
 
